28f2ec0476ba2a80557fb95004d04526910c976c
Pascal <pascal.charreix@gmail.com>
2014-12-09 11:58:42 +0100

    maj index + modules

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var http = require('http');
-
-http.createServer(function (req, res)
+var server = http.createServer(function (req, res)
 {
     console.log('coucou : ', req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('<b>Hello World ! :D</b>');
 }).listen(1234);
+var io = require('socket.io')(server);
+io.on('connection', function (socket)
+{
+	console.log('new connection');
+	setInterval(function()
+	{
+		socket.emit('ping');
+	}, 1000);
+	socket.on('pong', function()
+	{
+		console.log('ok');
+	});
+});
