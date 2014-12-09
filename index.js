bcc1acfe50084ecdbb9e57e650ff19ca197789e5
Pascal <pascal.charreix@gmail.com>
2014-12-09 11:21:34 +0100

    maj

---
+++ index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res)
+{
+    console.log('coucou : ', req.url);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<b>Hello World ! :D</b>');
+}).listen(1234);
